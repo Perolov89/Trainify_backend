f588033c7bc40ae09a203d41bdedd3e39564451d
Olle <olle.staalberg@gmail.com>
2024-12-24 08:58:20 +0100

    Added create tables in the db setup

--- db_setup.py
+++ db_setup.py
@@ -18,10 +18,10 @@ def get_connection():
     """
     return psycopg2.connect(
         dbname=DATABASE_NAME,
-        user="postgres",  # change if needed
+        user="postgres",
         password=PASSWORD,
-        host="localhost",  # change if needed
-        port="5432",  # change if needed
+        host="localhost",
+        port="5432",
     )
 
 
@@ -31,30 +31,82 @@ def create_tables():
     """
     connection = get_connection()
 
-    user_table = """ CREATE TABLE IF NOT EXISTS users(
+    # Define the SQL statements to create the tables
+    user_table = """ 
+    CREATE TABLE IF NOT EXISTS users (
         user_id SERIAL PRIMARY KEY,
-        password VARCHAR(50),
-        name VARCHAR(50) UNIQUE,
-        weight BIGINT,
+        password VARCHAR(100) NOT NULL,
+        name VARCHAR(250) UNIQUE NOT NULL,
+        weight BIGINT NOT NULL,
         user_record_id BIGINT,
         height BIGINT
-    )
+    );
     """
 
     categories_table = """
-    CREATE TABLE IF NOT EXISTS categories(
+    CREATE TABLE IF NOT EXISTS categories (
         category_id SERIAL PRIMARY KEY,
-        name VARCHAR(100) UNIQUE
-    )
+        name VARCHAR(100) UNIQUE NOT NULL
+    );
     """
 
+    exercises_table = """
+    CREATE TABLE IF NOT EXISTS exercises (
+        exercise_id SERIAL PRIMARY KEY,
+        exercise_name VARCHAR(250) NOT NULL,
+        exercise_weight BIGINT NOT NULL,
+        repmax_id BIGINT,
+        primary_muscle VARCHAR(100),
+        secondary_muscle VARCHAR(100),
+        category_id INT NOT NULL,
+        FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE
+    );
+    """
+
+    records_table = """
+    CREATE TABLE IF NOT EXISTS records (
+        record_id SERIAL PRIMARY KEY,
+        workout_id BIGINT NOT NULL,
+        user_id INT NOT NULL,
+        record_time TIMESTAMP NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    );
+    """
+
+    repmax_table = """
+    CREATE TABLE IF NOT EXISTS repmax (
+        repmax_id SERIAL PRIMARY KEY,
+        exercise_id INT NOT NULL,
+        user_id INT NOT NULL,
+        weight BIGINT NOT NULL,
+        FOREIGN KEY (exercise_id) REFERENCES exercises (exercise_id) ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    );
+    """
+
+    workouts_table = """
+    CREATE TABLE IF NOT EXISTS workouts (
+        workout_id SERIAL PRIMARY KEY,
+        workout_name VARCHAR(250) NOT NULL,
+        timecap BIGINT NOT NULL,
+        record_id INT NOT NULL,
+        exercise_id INT NOT NULL,
+        FOREIGN KEY (record_id) REFERENCES records (record_id) ON DELETE CASCADE,
+        FOREIGN KEY (exercise_id) REFERENCES exercises (exercise_id) ON DELETE CASCADE
+    );
+    """
+
+    # Execute the table creation statements
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(categories_table)
             cursor.execute(user_table)
-
+            cursor.execute(categories_table)
+            cursor.execute(exercises_table)
+            cursor.execute(records_table)
+            cursor.execute(repmax_table)
+            cursor.execute(workouts_table)
 
 if __name__ == "__main__":
-    # Only reason to execute this file would be to create new tables, meaning it serves a migration file
+    # Execute the script to create tables
     create_tables()
     print("Tables created successfully.")
